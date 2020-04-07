e78a129178dc45d533433bf980bbf70912390ad0
sbrack8t <sbrackat@forumone.com>
2020-04-07 09:12:34 -0400

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+const { series, src, dest, watch } = require("gulp");
+const sass = require("gulp-sass");
+const fs = require("fs");
+const browserSync = require("browser-sync").create();
+
+// Create Twing environment
+const { TwingEnvironment, TwingLoaderFilesystem } = require("twing");
+let loader = new TwingLoaderFilesystem("./");
+let twing = new TwingEnvironment(loader);
+
+// Build Twig Templates
+function buildTemplates(cb) {
+  fs.readFile("./data/data.json", (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    fs.mkdir("dist", { recursive: true }, (err) => {
+      if (err) {
+        throw err;
+      }
+
+      twing.render("index.twig", JSON.parse(data)).then((output) => {
+        // Write to html file
+        fs.writeFile("dist/index.html", output, function (err) {
+          if (err) {
+            console.log("error", err);
+          }
+        });
+      });
+    });
+  });
+
+  // Callback function
+  cb();
+}
+
+function buildStyles() {
+  return src("scss/style.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("./dist/css"))
+    .pipe(browserSync.stream());
+}
+
+function devBrowser(cb) {
+  browserSync.init({
+    server: {
+      baseDir: "./dist",
+    },
+  });
+  cb();
+}
+
+function watchFiles() {
+  watch(["scss/**/*.scss"], { usePolling: true, interval: 1500 }, buildStyles);
+  watch(
+    ["templates/**/*.twig", "index.twig"],
+    { usePolling: true, interval: 1500 },
+    buildTemplates
+  );
+  watch("dist/*.html").on("change", browserSync.reload);
+}
+
+exports.watchFiles = watchFiles;
+exports.devBrowser = devBrowser;
+exports.buildTemplates = buildTemplates;
+exports.buildStyles = buildStyles;
+exports.default = series(buildTemplates, buildStyles, devBrowser, watchFiles);
